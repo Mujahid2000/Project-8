cdd008df630ee51b96301d580665190fe4b41ad6
Mujahid2000 <developermujahid2001@gmail.com>
2023-09-27 23:44:57 +0600

    update classname

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -5,7 +5,7 @@ const Cards = ({ cards }) => {
 
     return (
 
-        <div className="grid   md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 mt-9 pb-10 px-5">
+        <div className="grid  md:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-9 pb-10 px-5">
             {
                 cards?.map(card => <Card key={card.id} cards={card}></Card>)
 
